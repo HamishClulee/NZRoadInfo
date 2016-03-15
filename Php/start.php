c40cc31907ad3ad97a71c8d9395b0384b6fad7e3
Hamish Clulee <hamish.clulee@hotmail.com>
2016-03-15 13:54:16 +1300

    Front end improvements. Welcome and Contact pages added. Google reCaptcha added.

--- Php/start.php
+++ Php/start.php
@@ -2,32 +2,32 @@
 
 echo "START.php fired! \n";
 
-//$startCH_Time = microtime(true);
-//
-//$curl_h = curl_init('https://infoconnect1.highwayinfo.govt.nz/ic/jbi/TREIS/REST/FeedService/');
-//curl_setopt($curl_h, CURLOPT_HTTPHEADER, array(
-//    'username: mjwynyard', 'password: Copper2004'
-//        )
-//);
-//curl_setopt($curl_h, CURLOPT_RETURNTRANSFER, true);
-//$response = curl_exec($curl_h);
-//
-//file_put_contents("../XML/SHClosures.xml", $response);
-//
-//
-//$curl_chch = curl_init('https://infoconnect1.highwayinfo.govt.nz/ic/jbi/TMP/REST/FeedService/');
-//
-//curl_setopt($curl_chch, CURLOPT_HTTPHEADER, array(
-//    'username: mjwynyard', 'password: Copper2004'
-//        )
-//);
-//curl_setopt($curl_chch, CURLOPT_RETURNTRANSFER, true);
-//$responseCH = curl_exec($curl_chch);
-//file_put_contents("../XML/chchTraffic.xml", $responseCH);
-//
-//$endCH_Time = microtime(true);
-//
-//echo "xml downloaded: " .($endCH_Time - $startCH_Time) ." seconds\n\n";
+$startCH_Time = microtime(true);
+
+$curl_h = curl_init('https://infoconnect1.highwayinfo.govt.nz/ic/jbi/TREIS/REST/FeedService/');
+curl_setopt($curl_h, CURLOPT_HTTPHEADER, array(
+    'username: REPLACE', 'password: REPLACE'
+        )
+);
+curl_setopt($curl_h, CURLOPT_RETURNTRANSFER, true);
+$response = curl_exec($curl_h);
+
+file_put_contents("../XML/SHClosures.xml", $response);
+
+
+$curl_chch = curl_init('https://infoconnect1.highwayinfo.govt.nz/ic/jbi/TMP/REST/FeedService/');
+
+curl_setopt($curl_chch, CURLOPT_HTTPHEADER, array(
+    'username: REPLACE', 'password: REPLACE'
+        )
+);
+curl_setopt($curl_chch, CURLOPT_RETURNTRANSFER, true);
+$responseCH = curl_exec($curl_chch);
+file_put_contents("../XML/chchTraffic.xml", $responseCH);
+
+$endCH_Time = microtime(true);
+
+echo "xml downloaded: " .($endCH_Time - $startCH_Time) ." seconds\n\n";
 
 
 include "SHXMLToJSON.php";
